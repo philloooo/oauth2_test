335c7dbb701ae1b4f5d193ecc5537e9f35babf4e
Ubuntu <ubuntu@ip-172-31-45-180.us-west-2.compute.internal>
2015-01-15 16:04:06 +0000

    Added anti-forgery token when redirecting user to google oauth

--- osdc_test/auth/views.py
+++ osdc_test/auth/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render,redirect
 import settings
 from django.http import HttpResponse
-
+from django.utils.crypto import get_random_string
 import urllib,json,requests
 # Create your views here.
 def index(request):
+    state=get_random_string(length=32)
+    parameters=settings.oauth['parameters'].copy()
+    parameters['state']=state
+    request.session['state']=state
     return redirect(settings.oauth['auth_uri']+"?"+\
-        urllib.urlencode(settings.oauth['parameters']))
+        urllib.urlencode(parameters))
 
 
 def oauth2callback(request):
-    return HttpResponse(json.dumps(request.GET.items()),content_type='application/json')
+    if request.session.get('state','')==request.GET['state']:
+        return HttpResponse(json.dumps(request.GET.items()),content_type='application/json')
+    else:
+        return HttpResponse(json.dumps({"error":"aa"}))
 
 
